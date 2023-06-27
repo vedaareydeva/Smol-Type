21105281187d2efdb2f6baeadb8c1c8ee75cf936
vedareydeva <vaidehi.deshmukh9@gmail.com>
2023-06-27 20:54:44 +0530

    Tailwind CSS setup.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Smol Type</h1>
+    <div className="h-screen bg-white-200 flex justify-center items-center">
+      <h1
+      className = "text-3xl font-bold"
+      >SmolType</h1>
     </div>
   );
 }
