6f8cf73473d242b2c77b2f1fee90dd4818bb58b2
vedareydeva <vaidehi.deshmukh9@gmail.com>
2023-06-29 23:14:46 +0530

    Make the registration screen's frontend.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";  //import necessary components from package
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react'     //import react module
 
 function Register() {
+  const [user, setUser] = React.useState({    //user is a state variable, useState is a hook
+    name: '',
+    email: '',
+    password: '',
+  })
   return (
-    <div>Register</div>
+    <div className='h-screen bg-primary flex items-center justify-center'>
+
+      <div className='bg-white shadow-md p-5 flex flex-col gap-5 w-96'>
+        <h1 className='text-2xl font-semibold text-orange-800 '>SmolType REGISTER</h1>
+        <hr />
+        <input type="text"
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          placeholder="Enter your name"
+        />
+        <input type="text"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter your email"
+        />
+        <input type="text"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Enter your password"
+        />
+
+        <button
+          className="contained-btn">
+          Register</button>
+      </div>
+
+    </div>
   )
 }
 
