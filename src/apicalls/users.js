f34bbac013314b0ef7842967c8e743abb2d0dbc3
vedareydeva <vaidehi.deshmukh9@gmail.com>
2023-07-19 23:29:29 +0530

    authorization pt1

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -21,3 +21,12 @@ export const RegisterUser = async (user) => {
     }
 
 };
+
+export const GetCurrentUser = async () => {
+    try {
+        const response = await axiosInstance.get("/api/users/get-current-user");
+        return response.data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
