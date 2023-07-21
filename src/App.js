2294a888b315d1e10c87c1d6ff0fd1f4bc16c838
vedareydeva <vaidehi.deshmukh9@gmail.com>
2023-07-21 22:26:01 +0530

    trying to make this work ;-;

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 
@@ -12,7 +13,7 @@ function App() {
       <Toaster position="top-center" reverseOrder={false} />
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { GetCurrentUser } from '../apicalls/users';
+import { useNavigate } from 'react-router-dom';
 
-function ProtectedRoute() {
-  return (
-    <div>ProtectedRoute</div>
-  )
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  const getCurrentUser = async () => {
+    try {
+      const response = await GetCurrentUser();
+      if (response.success) {
+        return true;
+      } else {
+        navigate('/login');
+        return false;
+      }
+    } catch (error) {
+      navigate('/login');
+    }
+  };
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (localStorage.getItem("token")) {
+        try {
+          await getCurrentUser();
+        } catch (error) {
+          // Handle any errors that might occur in getCurrentUser.
+        }
+      }
+    };
+
+    checkUser();
+  }, []);
+
+  return <div>{children}</div>;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
